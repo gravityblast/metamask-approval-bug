0e31171b4291e62e5c3781c726a546af576e68d7
Andrea Franz <andrea@gravityblast.com>
2023-04-30 03:54:02 +0200

    update logs

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let logEl;
 
 const log = (s) => {
   const curr = logEl.innerText;
-  logEl.innerText = curr + "\n" + s;
+  logEl.innerHTML = curr + "\n" + s;
 };
 
-const clearLog = (s) => {
+const clearLog = () => {
   logEl.innerText = "";
 };
 
@@ -23,18 +23,23 @@ window.addEventListener("load", async () => {
 
     log("connected");
 
+    const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
+    const abi = ["function approve(address,uint256)", "function allowance(address,address) view returns(uint256)"];
+    const contract = new ethers.Contract(daiAddress, abi).connect(signer);
+    const roundVotingStrategyAddress =
+      "0x10Cb6dEf9B7Eb5cEFA5E14caBAb1109fFf16B7B9";
+
+    const currentAllowance = await contract.allowance(signer.address, roundVotingStrategyAddress);
+    console.log(currentAllowance);
+    log(`currentAllowance: ${ethers.formatUnits(currentAllowance.toString())}<b>ok</b>`);
+
     btnApprove.addEventListener("click", async (e) => {
       clearLog();
       e.preventDefault();
-      const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
-      const roundVotingStrategyAddress =
-        "0x10Cb6dEf9B7Eb5cEFA5E14caBAb1109fFf16B7B9";
-      const abi = ["function approve(address,uint256)"];
       log(`waiting for tx signature...`);
-      const contract = new ethers.Contract(daiAddress, abi).connect(signer);
       const oneDai = ethers.parseUnits("1", "ether");
       const tx = await contract.approve(roundVotingStrategyAddress, oneDai);
-      log(`tx sent ${tx.hash}`);
+      log(`tx sent <a href="https://etherscan.io/tx/${tx.hash}" target="_blank">${tx.hash}</a>`);
       log(`waiting...`);
       const rec = await tx.wait();
       log(`gasUsed: ${rec.gasUsed}`);
